0d78751e1c1a8876b8436be07a3f5ef234f70e9b
Daniel Terry <24374442@studentmail.ucol.ac.nz>
2026-04-20 09:37:58 +1200

    Add initial project structure and skeleton files

---
+++ MovieLibraryApp/Models/Movie.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace MovieLibraryApp.Models
+{
+    public class Movie
+    {
+        public string MovieID { get; set; }
+        public string Title { get; set; }
+        public string Director { get; set; }
+        public string Genre { get; set; }
+        public int ReleaseYear { get; set; }
+        public bool IsAvailable { get; set; } = true;
+    }
+}
---
+++ MovieLibraryApp/ViewModels/MovieLibraryViewModel.cs
@@ -0,0 +1,70 @@
+using System;
+using System.Collections.Generic;
+using MovieLibraryApp.Models;
+
+namespace MovieLibraryApp.ViewModels
+{
+    public class MovieLibraryViewModel
+    {
+        public LinkedList<Movie> Movies { get; set; } = new LinkedList<Movie>();
+        public Dictionary<string, Movie> MovieHashtable { get; set; } = new Dictionary<string, Movie>();
+        public Dictionary<string, Queue<string>> BorrowQueues { get; set; } = new Dictionary<string, Queue<string>>();
+
+        public void AddMovie(Movie movie)
+        {
+            if (!MovieHashtable.ContainsKey(movie.MovieID))
+            {
+                Movies.AddLast(movie);
+                MovieHashtable[movie.MovieID] = movie;
+                BorrowQueues[movie.MovieID] = new Queue<string>();
+            }
+        }
+
+        public List<Movie> SearchByTitle(string title)
+        {
+            // Linear search skeleton
+            List<Movie> results = new List<Movie>();
+            foreach (var movie in Movies)
+            {
+                if (movie.Title.Contains(title, StringComparison.OrdinalIgnoreCase))
+                    results.Add(movie);
+            }
+            return results;
+        }
+
+        public Movie SearchByID(string movieID)
+        {
+            // Hashtable lookup
+            return MovieHashtable.ContainsKey(movieID) ? MovieHashtable[movieID] : null;
+        }
+
+        public void BorrowMovie(string movieID, string user)
+        {
+            if (MovieHashtable.ContainsKey(movieID))
+            {
+                var movie = MovieHashtable[movieID];
+                if (movie.IsAvailable)
+                    movie.IsAvailable = false;
+                else
+                    BorrowQueues[movieID].Enqueue(user);
+            }
+        }
+
+        public void ReturnMovie(string movieID)
+        {
+            if (MovieHashtable.ContainsKey(movieID))
+            {
+                var movie = MovieHashtable[movieID];
+                if (BorrowQueues[movieID].Count > 0)
+                {
+                    // Assign to next user in queue
+                    var nextUser = BorrowQueues[movieID].Dequeue();
+                    // Notify user (placeholder)
+                    Console.WriteLine($"Movie assigned to {nextUser}");
+                }
+                else
+                    movie.IsAvailable = true;
+            }
+        }
+    }
+}
